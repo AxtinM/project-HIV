43c9394d0ea117a74fcaff40c195d24fe2b43613
Ax <attigmohammed@gmail.com>
2022-12-02 07:19:54 +0100

    fix this fucking error

--- src/Routes/HomeRoute.jsx
+++ src/Routes/HomeRoute.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Footer from "../Components/Footer";
 import Header from "../Components/Header";
 
